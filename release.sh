d01a6a0dac3cf32b2510573f063ffcd8b144a7e8
hurongliang <hurongliang@gmail.com>
2025-06-26 15:22:12 +0800

    release 1.1.0

--- release.sh
+++ release.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
+
+# Clean up previous builds
 rm -rf ./build ./dist ./pyhurl.egg-info
-pip install setuptools wheel twine
-python setup.py sdist bdist_wheel
+
+# Install/upgrade build tools
+pip install --upgrade pip
+pip install --upgrade build twine
+
+# Build the package
+python -m build
+
+# Upload to PyPI
 twine upload dist/*
+
+# Clean up
+rm -rf ./build ./pyhurl.egg-info
